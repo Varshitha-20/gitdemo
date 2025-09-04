d79224e88bce56d65f367bfce8c0caec0ed5ba42
Varshitha-20 <137619072+Varshitha-20@users.noreply.github.com>
2025-09-04 12:22:11 +0530

    Update Welcom.java

--- gitdemo/src/gitdemo/Welcom.java
+++ gitdemo/src/gitdemo/Welcom.java
@@ -6,6 +6,9 @@ public class Welcom {
 		System.out.println("welcome to git");
 		System.out.println("welcome to gitdemo");
 		System.out.println("pushing the code to github");
+		System.out.println("pullin g the code to git");
+		System.out.println("pullin g the code to origin");
+		
 		
 	}
 
